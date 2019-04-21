ee91119d590ed42b6f91caf024f57aee2b743e9f
Andrew Silva <andrewsilva9@gmail.com>
2019-04-21 01:52:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase = "hello"
-    "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase = "HELLO"
-    "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
