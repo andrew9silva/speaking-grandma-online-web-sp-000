fbaf969f953ca90d305d5f5dbb7972eb9c6e921a
Andrew Silva <andrewsilva9@gmail.com>
2019-04-21 01:53:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase = "hello"
     puts "HUH?! SPEAK UP, SONNY!"
-  else phrase = "HELLO"
+  elsif phrase = "HELLO"
     puts "NO, NOT SINCE 1938!"
+  else phrase = "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
